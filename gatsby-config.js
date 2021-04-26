8cf60f90d3f94e263977dfebdb1c85eec493e435
Manoj Ponugoti <jameslucky917@gmail.com>
2021-04-26 16:53:21 +0530

    testing gatsby fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,6 +95,16 @@ module.exports = {
             },
         },
         {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [
+                    `roboto mono`,
+                    `muli\:400,400i,700,700i`,
+                ],
+                display: "swap",
+            },
+        },
+        {
           resolve: `gatsby-plugin-netlify-cms`,
           options: {
             modulePath: `${__dirname}/src/cms/cms.js`,
